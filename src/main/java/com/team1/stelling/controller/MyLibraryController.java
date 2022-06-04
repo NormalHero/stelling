9ad82f32c6ff4736d23099e70a1f4021dad90d00
NormalHero <mystparkkorea@gmail.com>
2022-06-04 15:25:29 +0900

    upstream병합

--- src/main/java/com/team1/stelling/controller/MyLibraryController.java
+++ src/main/java/com/team1/stelling/controller/MyLibraryController.java
@@ -1,27 +1,20 @@
 package com.team1.stelling.controller;
 
 import com.team1.stelling.domain.dto.PageDTO;
-import com.team1.stelling.domain.repository.UserRepository;
-import com.team1.stelling.domain.vo.*;
-import com.team1.stelling.domain.dto.PageableDTO;
-import com.team1.stelling.domain.dto.PagingDTO;
-import com.team1.stelling.domain.vo.*;
-import com.team1.stelling.mapper.PayMapper;
+import com.team1.stelling.domain.vo.Criteria;
+import com.team1.stelling.domain.vo.PayVO;
 import com.team1.stelling.service.PayService;
-import com.team1.stelling.service.SupportService;
-import com.team1.stelling.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.HandlerMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
-import java.util.ArrayList;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -31,9 +24,6 @@ import java.util.List;
 public class MyLibraryController {
 
     private final PayService payService;
-    private final SupportService supportService;
-    private final UserService userService;
-
 
     @RequestMapping("")
     public void myLibrary(){
@@ -60,16 +50,10 @@ public class MyLibraryController {
         log.info("myLibraryPick");
         return "myLibrary/myLibraryPick";
     }
-    
-    //코인샵 이동
-    @GetMapping("/coinShop")
-    public String coinShop(HttpServletRequest request, Model model){
-        HttpSession session = request.getSession();
-        Long userNumber = (Long)session.getAttribute("userNumber");
-        log.info("유저 세션 번호 : " + String.valueOf(userNumber) + "******************");
-        UserVO userVO = userService.findByUserNumber(userNumber);
 
-        model.addAttribute("userVO", userVO);
+    @GetMapping("/coinShop")
+    public String coinShop(){
+        log.info("coinShop");
         return "cash/coinShop";
     }
 
@@ -82,49 +66,28 @@ public class MyLibraryController {
 
     //결제 리스트(마이페이지)
     @GetMapping("/payList")
-    public String payList(Paging paging, Model model, HttpServletRequest request){
-        HttpSession session = request.getSession();
-        Long userNumber = (Long) session.getAttribute("userNumber");
-        log.info("userNumber + " + userNumber + "********");
-        NumberFormat numberFormat = NumberFormat.getInstance();
-        String payChargeTotal = null;
-        payChargeTotal = numberFormat.format(payService.getTotal(userNumber).getChargeTotal());
-        log.info("총 결제 금액" + payChargeTotal);
-        log.info(paging.toString());
-
-        int total = payService.getSearchTotal(paging);
-
-        PagingDTO pageMaker = new PagingDTO(paging, total);
-
-//        String startDate = criteria.getStartDate();
-//        String endDate = criteria.getEndDate();
-//        log.info("startDate : " + startDate);
-//        log.info("endDate : " + endDate);
-        model.addAttribute("payList", payService.getList(paging, userNumber));
-        model.addAttribute("pageMaker", pageMaker);
-        model.addAttribute("payChargeTotal", payChargeTotal);
-//        model.addAttribute("pageDTO", new PageDTO(paging, payService.getSearchTotal(paging)));
-        return "/myPage/myPagePayList";
+    public String payList(Long userNumber, Criteria criteria, Model model){
+            model.addAttribute("payList", payService.getList(criteria, userNumber));
+            model.addAttribute("pageDTO", new PageDTO(criteria, payService.getSearchTotal(criteria)));
+            model.addAttribute("payDTO", payService.getTotal(userNumber));
+        return "myPage/myPagePayList";
     }
 
-    @GetMapping("/supportList")
-    public String supportList(Paging paging, Model model, HttpServletRequest request){
-        HttpSession session = request.getSession();
-        Long supportSponser = (Long) session.getAttribute("userNumber");
-        NumberFormat numberFormat = NumberFormat.getInstance();
-        String supportCoinTotal = numberFormat.format(supportService.getSupportCoinTotal(supportSponser).getSupportTotal());
-        log.info("총 후원 코인" + supportCoinTotal);
-
-        int total = supportService.getSearchSupportTotal(paging);
-
-        PagingDTO pageMaker = new PagingDTO(paging, total);
-
-        model.addAttribute("supportList", supportService.getSupportList(paging, supportSponser));
-        model.addAttribute("pageMaker", pageMaker);
-        model.addAttribute("supportCoinTotal", supportCoinTotal);
-//        model.addAttribute("pageDTO", new PageDTO(criteria, supportService.getSearchSupportTotal(criteria)));
-        return "myPage/myPageSupportList";
-    }
-
+//    @GetMapping("/payList/{userNumber}")
+//    public String payList(@PathVariable Long userNumber, String startDate, String endDate, Model model){
+//        if(startDate == null & endDate == null) {
+//            model.addAttribute("payList", payService.getList(userNumber));
+//        }
+//        return "myPage/myPagePayList";
+//    }
+    //날짜 검색(마이페이지)
+//    @GetMapping("/dateSelect")
+//    public void dateSelect(String startDate, String endDate)
+    //결제 내역 등록
+//    @PostMapping("/register/{userNumber}/{payCharge}/{payCoinCount}")
+//    public String register(@PathVariable Long userNumber, @PathVariable Long payCharge, @PathVariable Long payCoinCount){
+//        payService.register(userNumber, payCharge, payCoinCount);
+//        return "cash/coinShop";
+//    }
 
 }
--- src/main/java/com/team1/stelling/service/PayService.java
+++ src/main/java/com/team1/stelling/service/PayService.java
@@ -2,7 +2,6 @@ package com.team1.stelling.service;
 
 import com.team1.stelling.domain.dao.PayDAO;
 import com.team1.stelling.domain.vo.Criteria;
-import com.team1.stelling.domain.vo.Paging;
 import com.team1.stelling.domain.vo.PayDTO;
 import com.team1.stelling.domain.vo.PayVO;
 import lombok.RequiredArgsConstructor;
@@ -19,15 +18,13 @@ public class PayService{
 
 //    public PayVO get(Long payNumber) {return payDAO.get(payNumber);}
     //결제 내역
-    public List<PayVO> getList(Paging paging, Long userNumber) {return payDAO.getList(paging, userNumber);}
+    public List<PayVO> getList(Criteria criteria, Long userNumber) {return payDAO.getList(criteria, userNumber);}
 
     //총 결제 금액
     public PayDTO getTotal(Long userNumber) {return payDAO.getTotal(userNumber);}
 
-    public List<PayVO> getPayCharge(Long userNumber) {return payDAO.getPayCharge(userNumber);}
-
     //검색한 결과의 총 개수(페이징 처리를 위함)
-    public int getSearchTotal(Paging paging) {return payDAO.getSearchTotal(paging);}
+    public int getSearchTotal(Criteria criteria) {return payDAO.getSearchTotal(criteria);}
 
 //    public void register(Long userNumber, Long payCharge, Long payCoinCount) {payDAO.register(userNumber, payCharge, payCoinCount);}
     public void register(PayVO payVO) {payDAO.register(payVO);}
